a103821b87c36877f65f724390e4f7cb586f60f3
LucasSilvvaLLima <lucas.lima@sptech.school>
2023-02-09 19:34:50 -0300

    Configuração de ambiente e primeiro contato

---
+++ primeiro-projeto-java-ads-c/src/main/java/lucas/primeiro/projeto/java/ads/c/PrimeiraClasse.java
@@ -0,0 +1,29 @@
+// PADRÃO DE NOMENCLATURA PROJETO: kebabe-case (Projeto)
+package lucas.primeiro.projeto.java.ads.c;
+
+/**
+ *
+ * @author lucka
+ */
+// CLASSES SÃO ARQUIVOS QUE GUARDAM NOSSO CÓDIGO EM JAVA
+// METODOS NO JAVA É EQUIVALENTE AO QUE CHAMAVAMOS DE FUNÇÕES NO JAVASCRIPT
+// ARGUMENTOS É EQUIVALENTE AO QUE CHAMAVAMOS DE PARAMETROS NO JAVASCRIPT
+
+// PADRÃO DE NOMENCLATURA CLASSE: PascalCase 
+public class PrimeiraClasse {
+//    ATALHO: MAIN + TAB OU PSVM + TAB
+
+    public static void main(String[] args) {
+//      CONSOLE.LOG EM JAVA
+//      ATALHO: SOUT + TAB
+        System.out.println("Olá Mundo!");
+
+//      PADRÃO DE NOMENCLATURA VARIAVEL: cameCase
+        String nomePadrao = "Lorena";
+
+        String nome = "Lucas";
+
+        System.out.println("Meu nome é: " + nome);
+        System.out.println("Nome padrão é: " + nomePadrao);
+    }
+}
