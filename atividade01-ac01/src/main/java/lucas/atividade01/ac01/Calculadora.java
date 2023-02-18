5da4865d94073b2094dda17f58c54a496a80e4e7
LucasSilvvaLLima <lucas.lima@sptech.school>
2023-02-18 02:41:16 -0300

    1° Atividade avaliativa ac1 LP

---
+++ atividade01-ac01/src/main/java/lucas/atividade01/ac01/Calculadora.java
@@ -0,0 +1,92 @@
+package lucas.atividade01.ac01;
+
+import java.util.Scanner;
+
+/**
+ * @author lucka
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Integer operacaoDesejada = 0;
+        Integer primeiroValor = 0, segundoValor = 0, resultado = 0;
+        Double primeiroValorDouble = 0.0, segundoValorDouble = 0.0, resultadoDouble = 0.0;
+        String enunciado = "", tabuada = "";
+        do {
+            System.out.println("\n  Digite o número correspondente a operação desejada: \n"
+                    + "1 - Soma \n"
+                    + "2 - Multiplicação \n"
+                    + "3 - Divisão \n"
+                    + "4 - Subtração \n"
+                    + "5 - Potência \n"
+                    + "6 - Resto \n"
+                    + "0 – Sair");
+            operacaoDesejada = entrada.nextInt();
+
+            switch (operacaoDesejada) {
+                case 0:
+                    break;
+                case 1:
+                    System.out.print("Digite um número inteiro p/ SOMAR: ");
+                    enunciado = "%d + %d = %d";
+                    primeiroValor = entrada.nextInt();
+                    break;
+                case 2:
+                    System.out.print("Digite um número inteiro p/ MULTIPLICAR: ");
+                    enunciado = "%d x %d = %d";
+                    primeiroValor = entrada.nextInt();
+                    break;
+                case 3:
+                    System.out.print("Digite um número inteiro p/ DIVIDIR: ");
+                    enunciado = "%.2f / %.2f = %.2f";
+                    primeiroValorDouble = entrada.nextDouble();
+                    break;
+                case 4:
+                    System.out.print("Digite um número inteiro / SUBTRAIR: ");
+                    enunciado = "%d - %d = %d";
+                    primeiroValor = entrada.nextInt();
+                    break;
+                case 5:
+                    System.out.print("Digite um número inteiro p/ obter a POTÊNCIA: ");
+                    enunciado = "%d elevado a %d = %d";
+                    primeiroValor = entrada.nextInt();
+                    resultado = primeiroValor;
+                    break;
+                case 6:
+                    System.out.print("Digite um número inteiro p/ obter o RESTO da divisão: ");
+                    enunciado = "%d dividido por %d sobra %d";
+                    primeiroValor = entrada.nextInt();
+                    break;
+                default:
+                    System.out.println("\nOPÇÃO INVALIDA, TENTE NOVAMENTE");
+            }
+            if (operacaoDesejada.equals(0)) {
+                System.out.println("Até logo!");
+            } else if (operacaoDesejada <= 6) {
+                for (int i = 1; i <= 10; i++) {
+                    segundoValor = i;
+                    segundoValorDouble = i + 0.0;
+                    if (operacaoDesejada.equals(1)) {
+                        resultado = primeiroValor + segundoValor;
+                    } else if (operacaoDesejada.equals(2)) {
+                        resultado = primeiroValor * segundoValor;
+                    } else if (operacaoDesejada.equals(3)) {
+                        resultadoDouble = primeiroValorDouble / segundoValorDouble;
+                    } else if (operacaoDesejada.equals(4)) {
+                        resultado = primeiroValor - segundoValor;
+                    } else if (operacaoDesejada.equals(6)) {
+                        resultado = primeiroValor % segundoValor;
+                    }
+                    if (!operacaoDesejada.equals(3)) {
+                        tabuada = String.format(enunciado, primeiroValor, segundoValor, resultado);
+                        resultado = primeiroValor * resultado;
+                    } else {
+                        tabuada = String.format(enunciado, primeiroValorDouble, segundoValorDouble, resultadoDouble);
+                    }
+                    System.out.println(tabuada);
+                }
+            }
+        } while (!operacaoDesejada.equals(0));
+    }
+}
